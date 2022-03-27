65a6e9b2b6670da518b00899b5179f68c6668682
dev-yakuza <dev.yakuza@gmail.com>
2022-03-27 20:38:06 +0900

    Add MUI example

---
+++ 8.mui/my-app/stories/SampleButton.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@mui/material';
+
+interface Props {
+  readonly color?: 'primary' | 'secondary' | undefined;
+  readonly backgroundColor?: string;
+  readonly size?: 'small' | 'medium' | 'large';
+  readonly label: string;
+  readonly onClick?: () => void;
+}
+
+export const SampleButton = ({
+  color = 'primary',
+  size = 'medium',
+  backgroundColor,
+  label,
+  onClick,
+}: Props) => {
+  return (
+    <Button
+      variant="contained"
+      color={color}
+      size={size}
+      style={{ backgroundColor }}
+      onClick={onClick}>
+      {label}
+    </Button>
+  );
+};
