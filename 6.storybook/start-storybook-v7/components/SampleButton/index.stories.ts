c99be8daf9f52872035e0bcee434173cfa4518e0
dev-yakuza <dev.yakuza@gmail.com>
2023-04-03 13:54:44 +0900

    Add storybook v7 exmaple

---
+++ 6.storybook/start-storybook-v7/components/SampleButton/index.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { SampleButton } from '.';
+
+const meta: Meta<typeof SampleButton> = {
+  title: 'Sample/Button',
+  component: SampleButton,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: {
+      control: 'color',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SampleButton>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
---
+++ 6.storybook/start-storybook-v7/components/SampleButton/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './index.css';
+
+interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const SampleButton = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
+      {...props}>
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
